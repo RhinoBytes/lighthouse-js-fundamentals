f87a688599acc98bfaa3dd443ec856b621a74e29
Doug Ross <dross.canada@gmail.com>
2023-01-21 12:00:19 -0500

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+const raining = true;
+const cold = true; 
+const temperature = -5;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15 ) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now your ready to go outside!");
+
+if (raining) {
+  console.log("Dont forget your umbrella!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+if (cold) {
+  console.log("Make sure you pick you a scarf!");
+} else {
+
+}
+
+console.log("Now your ready to go outside!");
