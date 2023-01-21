4c7f823d2c96c28ebf447e9f28325b77c888ad29
Doug Ross <dross.canada@gmail.com>
2023-01-21 12:05:50 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -25,3 +25,21 @@ if (cold) {
 }
 
 console.log("Now your ready to go outside!");
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligable to vote.")
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...")
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!")
+}
+
