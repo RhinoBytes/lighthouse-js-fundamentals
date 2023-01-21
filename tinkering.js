ba0ac8b607c37ab5d06a2e7f49d47684a0b18352
Doug Ross <dross.canada@gmail.com>
2023-01-21 12:07:43 -0500

    added other exercise files

---
+++ tinkering.js
@@ -0,0 +1,18 @@
+function multiply(a, b) {
+  return a+b;
+}
+
+const result = multiply(2, 5);
+
+console.log(result);
+
+
+console.log("The man whispered, \t \"please speak to me.\"")
+
+var my_string = "udacity"
+for (var i =0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}
+
+
+"green" == "blue"
