af1ab3328ca2812909be9381101377bba5859b14
Doug Ross <dross.canada@gmail.com>
2023-01-28 17:48:36 -0500

    added ingredient loop

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let n = 0; 
+while (n < ingredients.length) {
+  
+  n++;
+}
+console.log(ingredients);
+
+// Write a for loop that prints out the contents of ingredients:
+for (let x = 0; x < ingredients.length; x++) {
+  
+}
+console.log(ingredients);
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (let i = 0; i < ingredients.length; i++) {
+  
+}
+console.log(ingredients.reverse());
